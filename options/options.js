b3c53a93d3687ac52fd5d351617b7d783b311162
Crowdbotics <team@crowdbotics.com>
2021-08-18 16:06:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aed_medical_29914Identifier",
+  url: "https://aed_medical_29914Identifier.botics.co",
+  api: "https://aed_medical_29914Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
